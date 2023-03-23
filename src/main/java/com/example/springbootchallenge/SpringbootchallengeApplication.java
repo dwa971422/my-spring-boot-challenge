e2e8719ba341286bcf4556a1855e73a7d794f1a3
Shih Yao Wang <sywang@sykenwang-MBA.local>
2023-03-22 20:03:32 -0500

    Initial commit

---
+++ src/main/java/com/example/springbootchallenge/SpringbootchallengeApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootchallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootchallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootchallengeApplication.class, args);
+	}
+
+}
